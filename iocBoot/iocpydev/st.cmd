bb431097fd087070a87128b4f0fa4e99b433e265
Klemen Vodopivec <kvx@ornl.gov>
2020-01-03 08:00:41 -0500

    Added support for input records
    A bit of code restructuring in order to support reading values from
    Python into input records. Still pretty much templatized.

--- iocBoot/iocpydev/st.cmd
+++ iocBoot/iocpydev/st.cmd
@@ -2,13 +2,14 @@
 
 < envPaths
 
+# PYTHONPATH points to folders where Python modules are.
+epicsEnvSet("PYTHONPATH","$(TOP)/python")
+
 cd ${TOP}
 
 ## Register all support components
 dbLoadDatabase "${TOP}/dbd/pydevioc.dbd"
 pydevioc_registerRecordDeviceDriver pdbbase
-
-#ipmiConnect IPMI1 192.168.1.252 "" "" "none" "lan" "operator"
 
 ## Load record instances
 dbLoadRecords("${TOP}/db/test.db")
--- src/pymodule.cpp
+++ src/pymodule.cpp
@@ -26,22 +26,121 @@ extern "C"
     }
 };
 
+static void decodePyError(std::string &type, std::string &text, std::string &trace)
+{
+    PyObject *excType, *excValue, *excTrace;
+    PyErr_Fetch(&excType, &excValue, &excTrace);
+    if (excType != NULL)
+    {
+        PyObject *str = PyObject_Repr(excType);
+        type = PyString_AsString(str);
+        Py_DecRef(str);
+        Py_DecRef(excType);
+    }
+    if (excValue != NULL)
+    {
+        PyObject *str = PyObject_Repr(excValue);
+        text = PyString_AsString(str);
+        Py_DecRef(str);
+        Py_DecRef(excValue);
+    }
+    if (excTrace != NULL)
+    {
+        trace = "pydevice: not yet implemented";
+        // TODO: Found this post but it's using Python internals and doesn't compile
+        //       https://stackoverflow.com/a/43984515
+        Py_DecRef(excTrace);
+    }
+}
+
 template <typename T>
-static PyObject *convertLiteral(T val)
+static PyObject *convertToNative(T val)
 {
-    return PyInt_FromLong(val);
+    PyErr_Clear();
+    auto ret = PyInt_FromLong(val);
+    if (PyErr_Occurred())
+    {
+        std::string type, text, trace;
+        decodePyError(type, text, trace);
+        throw std::runtime_error("Can not convert argument from 'long' type: " + text);
+    }
+    return ret;
 }
 
 template <>
-PyObject *convertLiteral(double val)
+PyObject *convertToNative(double val)
 {
-    return PyFloat_FromDouble(val);
+    PyErr_Clear();
+    auto ret = PyFloat_FromDouble(val);
+    if (PyErr_Occurred())
+    {
+        std::string type, text, trace;
+        decodePyError(type, text, trace);
+        throw std::runtime_error("Can not convert argument from 'double' type: " + text);
+    }
+    return ret;
 }
 
 template <>
-PyObject *convertLiteral(char* val)
+PyObject *convertToNative(char *val)
 {
-    return PyString_FromString(val);
+    PyErr_Clear();
+    auto ret = PyString_FromString(val);
+    if (PyErr_Occurred())
+    {
+        std::string type, text, trace;
+        decodePyError(type, text, trace);
+        throw std::runtime_error("Can not convert argument from 'char*' type: " + text);
+    }
+    return ret;
+}
+
+template <typename T>
+T convertFromNative(PyObject* val)
+{
+    PyErr_Clear();
+    T ret = PyInt_AsLong(val);
+    if (PyErr_Occurred())
+    {
+        std::string type, text, trace;
+        decodePyError(type, text, trace);
+        throw std::runtime_error("Can not convert return code to 'long' type: " + text);
+    }
+    return ret;
+}
+
+template <>
+bool convertFromNative(PyObject *val)
+{
+    return false;
+}
+
+template <>
+double convertFromNative(PyObject *val)
+{
+    PyErr_Clear();
+    double ret = PyFloat_AsDouble(val);
+    if (PyErr_Occurred())
+    {
+        std::string type, text, trace;
+        decodePyError(type, text, trace);
+        throw std::runtime_error("Can not convert return code to 'double' type: " + text);
+    }
+    return ret;
+}
+
+template <>
+char* convertFromNative(PyObject* val)
+{
+    PyErr_Clear();
+    char* ret = PyString_AsString(val);
+    if (PyErr_Occurred())
+    {
+        std::string type, text, trace;
+        decodePyError(type, text, trace);
+        throw std::runtime_error("Can not convert return code to 'char*' type: " + text);
+    }
+    return ret;
 }
 
 PyModule::PyModule(const std::string &id)
@@ -67,7 +166,10 @@ PyModule::PyModule(const std::string &id)
     m_module = module;
 
     std::string threadName = "thread_" + id;
-    epicsThreadCreate(threadName.c_str(), epicsThreadPriorityLow, epicsThreadStackMedium, (EPICSTHREADFUNC)&ThreadC, this);
+    epicsThreadCreate(threadName.c_str(),
+                      epicsThreadPriorityLow,
+                      epicsThreadGetStackSize(epicsThreadStackMedium),
+                      (EPICSTHREADFUNC)&ThreadC, this);
 }
 
 PyModule::~PyModule()
@@ -80,44 +182,17 @@ PyModule::~PyModule()
     }
 }
 
-template <typename T>
-void PyModule::schedule(const std::string& func, T arg, Callback &cb)
+void PyModule::schedule(Task& task)
 {
-    auto funcPy = PyObject_GetAttrString(m_module, func.c_str());
-    if (!funcPy)
-    {
-        throw std::invalid_argument("No such function!");
-    }
-    if (!PyCallable_Check(funcPy))
-    {
-        Py_XDECREF(funcPy);
-        throw std::invalid_argument("Python object not a function!");
-    }
-
-    auto argPy = convertLiteral<T>(arg);
-    if (!argPy)
-    {
-        Py_XDECREF(funcPy);
-        throw std::invalid_argument("Python object not a function!");
-    }
-    auto argsPy = PyTuple_Pack(1, argPy);
-
-    auto task = std::bind(&PyModule::exec, this, funcPy, argsPy, cb);
-
     m_mutex.lock();
     m_queue.emplace_back(task);
     m_mutex.unlock();
     m_event.signal();
 }
 
-// Instantiate schedule() for popular EPICS DB types
-template void PyModule::schedule(const std::string&, epicsFloat64, Callback&);
-template void PyModule::schedule(const std::string&, epicsInt32,   Callback &);
-template void PyModule::schedule(const std::string&, epicsUInt16,  Callback &);
-template void PyModule::schedule(const std::string&, char*,        Callback &);
-
 void PyModule::run()
 {
+    m_processing = true;
     while (m_processing)
     {
         m_mutex.lock();
@@ -133,64 +208,97 @@ void PyModule::run()
         m_queue.pop_front();
         m_mutex.unlock();
 
-        task();
+        task(*this);
     }
 
     m_stopped.signal();
 }
 
-void PyModule::exec(PyObject* func, PyObject* args, Callback& cb)
+template <typename R, typename A>
+R PyModule::exec(const std::string& func, A arg)
 {
-    bool success;
-    std::stringstream error;
-    auto ret = PyObject_CallObject(func, args);
-    Py_DECREF(args);
-    if (ret == nullptr)
+    auto funcPy = PyObject_GetAttrString(m_module, func.c_str());
+    if (!funcPy)
     {
-        success = false;
-        Py_DECREF(func);
+        throw std::invalid_argument("No such function!");
+    }
+    if (!PyCallable_Check(funcPy))
+    {
+        Py_DECREF(funcPy);
+        throw std::invalid_argument("Python object not a function!");
+    }
 
-        error << "Function failed:" << std::endl;
-
-        PyObject *excType, *excValue, *excTrace;
-        PyErr_Fetch(&excType, &excValue, &excTrace);
-        if (excType != NULL)
-        {
-            PyObject *str = PyObject_Repr(excType);
-            error << " - Exception type: " << PyString_AsString(str) << std::endl;
-            Py_DecRef(str);
-            Py_DecRef(excType);
-        }
-        if (excValue != NULL)
-        {
-            PyObject *str = PyObject_Repr(excValue);
-            error << " - Exception value: " << PyString_AsString(str) << std::endl;
-            Py_DecRef(str);
-            Py_DecRef(excValue);
-        }
-        if (excTrace != NULL)
-        {
-            // TODO: Found this post but it's using Python internals and doesn't compile
-            //       https://stackoverflow.com/a/43984515
-            Py_DecRef(excTrace);
-        }
+    PyObject* argsPy;
+    if (std::is_same<A, bool>::value)
+    {
+        argsPy = PyTuple_New(0);
     }
     else
     {
-        success = true;
-        Py_DECREF(ret);
+        PyObject *argPy;
+        try
+        {
+            argPy = convertToNative<A>(arg);
+        }
+        catch (...)
+        {
+            Py_DECREF(funcPy);
+            throw;
+        }
+        argsPy = PyTuple_Pack(1, argPy);
+        Py_DECREF(argPy);
     }
-    cb(success, error.str());
-}
-
-PyObject* PyModule::resolveFunction(const std::string& name)
-{
-    auto func = PyObject_GetAttrString(m_module, name.c_str());
-    if (!PyCallable_Check(func))
+    if (!argsPy)
     {
-        Py_XDECREF(func);
-        func = nullptr;
+        Py_DECREF(funcPy);
+        throw std::invalid_argument("Failed to initialize Python arguments object!");
     }
 
-    return func;
+    auto ret = PyObject_CallObject(funcPy, argsPy);
+    Py_DECREF(argsPy);
+
+    if (ret != nullptr)
+    {
+        // Success
+        Py_DECREF(ret);
+        return convertFromNative<R>(ret);
+    }
+    else
+    {
+        Py_DECREF(funcPy);
+
+        std::string type;
+        std::string text;
+        std::string trace;
+        decodePyError(type, text, trace);
+
+        std::string error;
+        error += "Function threw exception:\n";
+        error += " - Exception type: " + type + "\n";
+        error += " - Exception value: " + text + "\n";
+        throw std::runtime_error(error);
+    }
 }
+
+template <typename T>
+T PyModule::exec(const std::string& func)
+{
+    return exec<T, bool>(func, false);
+}
+
+template <typename T>
+void PyModule::exec(const std::string& func, T arg)
+{
+    (void)exec<bool, T>(func, arg);
+}
+
+// Instantiate schedule() for popular EPICS DB types
+template void PyModule::exec(const std::string&, epicsFloat64);
+template void PyModule::exec(const std::string&, epicsInt32);
+template void PyModule::exec(const std::string&, epicsUInt16);
+template void PyModule::exec(const std::string&, char*);
+
+template epicsFloat64 PyModule::exec(const std::string&);
+template epicsInt32   PyModule::exec(const std::string &);
+template epicsUInt16  PyModule::exec(const std::string &);
+template char*        PyModule::exec(const std::string &);
