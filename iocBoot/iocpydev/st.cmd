a7ec0834d86b1fa462023b0fa4ad2f27450c5536
Klemen Vodopivec <klemen@vodopivec.org>
2020-04-05 19:43:10 -0400

    Reworked example PVs provides in out-of-box IOC.

--- iocBoot/iocpydev/st.cmd
+++ iocBoot/iocpydev/st.cmd
@@ -1,4 +1,4 @@
-#!../../bin/linux-x86_64-debug/pydevioc
+#!../../bin/linux-x86_64/pydevioc
 
 < envPaths
 
@@ -16,8 +16,8 @@ dbLoadRecords("${TOP}/db/pydevtest.db")
 
 cd ${TOP}/iocBoot/${IOC}
 
+pydev("import time")
 pydev("import pydevtest")
 pydev("google = pydevtest.HttpClient('www.google.com', 80)")
-pydev("example = pydevtest.HttpClient('www.example.com', 80)")
 
 iocInit
--- python/pydevtest.py
+++ python/pydevtest.py
@@ -1,13 +1,7 @@
 import socket
-import random
-
-a = "Hello World from test!!!"
-
-def pow(a, b=2):
-    return a**b
 
 class HttpClient(object):
-    def __init__(self, host, port):
+    def __init__(self, host, port=80):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._addr = (host, port)
 
@@ -17,33 +11,23 @@ class HttpClient(object):
         self._socket.sendall(req)
         rsp = self._socket.recv(1024)
         self._socket.close()
+        try:
+            proto, code, msg = rsp.split("\r\n")[0].split(" ")
+            pydev.iointr('proto', proto)
+            pydev.iointr('code',  int(code))
+            pydev.iointr('msg',   msg)
+        except:
+            pass
         return rsp
-
-title = None
-def setTitle(text):
-    global title
-    title = text
-
-    pydev.iointr("title", title)
-
-def setArray(arr):
-    l = []
-    for i in range(random.randint(1,100)):
-        l.append(random.randint(0,255))
-    return l
-
-test = 0
-def setTest(val):
-    global test
-    test = val
-    pydev.iointr("test", test)
 
 
 if __name__ == "__main__":
     class pydev:
+        @staticmethod
         def iointr(name, value):
-            print("%s=%s" % (name, value))
-
-    s = HttpClient("www.google.com", 80)
-    print s.get("/")
+            print("pydev.iointr('%s', '%s')" % (name, value))
+    google = HttpClient("www.google.com")
+    rsp = google.get()
+    print("")
+    print(rsp)
 
