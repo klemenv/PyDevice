5fdf18c15292e3b0660e09dfb4001e0a9f878703
Vodopivec, Klemen <vodopiveck@ornl.gov>
2021-02-27 19:59:49 -0500

    Allow to build against Python 3.8+
    Starting with Python 3.8, the python-config needs additional --embed
    parameter when embedding into applications. So far it's only needed
    for the linker stage. Added automatic detection of Python 3.8+ to the
    IOC Makefile.
    
    Fixes #6

--- src/util.cpp
+++ src/util.cpp
@@ -27,22 +27,13 @@ std::map<std::string, std::string> getReplacables(const std::string& text)
 std::string replace(const std::string& text, const std::map<std::string, std::string>& fields)
 {
     std::string out = text;
-    auto pos1 = out.find('%');
-    auto pos2 = out.find('%', pos1+1);
 
-    while (pos1 != out.npos && pos2 != out.npos) {
-        std::string field = out.substr(pos1, pos2-pos1+1);
-        std::string val;
-        try {
-            val = fields.at(field);
-        } catch (std::out_of_range&) {
-            val = "";
+    for (auto& field: fields) {
+        auto pos = out.find(field.first);
+        while (pos != out.npos) {
+            out.replace(pos, pos+field.first.size(), field.second);
+            pos = out.find(field.first, pos+field.second.size());
         }
-
-        out.replace(pos1, pos2-pos1+1, val);
-
-        pos1 = out.find('%', pos1);
-        pos2 = out.find('%', pos1+1);
     }
 
     return out;
